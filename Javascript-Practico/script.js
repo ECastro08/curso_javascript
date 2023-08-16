bd052ddf6eef9963fc4c5de3c94c4951dfeeaa77
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-15 23:27:25 -0500

    se editaron los archivos html y js para hacer el procedimiento de escuchar a los usuarios con funciones como onClick

--- Javascript-Practico/script.js
+++ Javascript-Practico/script.js
@@ -1,42 +1,12 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btn-calculo')
+const pResult = document.querySelector('#resultado')
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-})
+function btnOnclick() {
+    const sumaInputs = console.log(input1.value + input2.value);
+    pResult.innerText = "Resultado: " + sumaInputs;
 
+}
 
-// escribiendo html desde js
-
-h1.innerHTML = "pato"   // cambio el texto de la etiqueta h1
-// 
-//console.log(h1.getAttribute('class'));
-// modificar la clase
-//h1.setAttribute('class','verde')  // modificamos la clase a verde
-
-// otra forma
-
-h1.classList.add('azul');  // agregamos una clase llamada azul
-h1.classList.remove('rojo');  // elimino la clase azul en el html
-
-
-// modificando el input value
-
-input.value = "45678";
-
-// crear un elemeno desde cero
-
-const img = document.createElement('img'); // creamos la imagen 
-img.setAttribute('src', 'https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=400');
-console.log(img);
-
-//pid.append(img);  // insertamos la imagen en el contenedor pid sin eliminar el contenido de pid
-gipid.replaceChildren(img);  // insertamos la imagen y borra el contenido de pid
-// document.createElement('span')
